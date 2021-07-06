2d96955bd7efa313efb186603dfafdf62d5b1047
lucascarneiro-dev <lucas.carneiro098@hotmail.com>
2021-07-06 19:37:39 -0300

    Update boomer.py

--- boomer.py
+++ boomer.py
@@ -1,17 +1,17 @@
-from character import Personagem
+# from character import Personagem
 
-class Boomer(Personagem):
-    def __init__(self, nome, idade, energia, dinheiro, ansiedade):
-        super().__init__(nome, idade, energia, dinheiro, ansiedade)
+# class Boomer(Personagem):
+#     def __init__(self, nome, idade, energia, dinheiro, ansiedade):
+#         super().__init__(nome, idade, energia, dinheiro, ansiedade)
 
 
-    def __str__(self):
-        return f'''
-        ESTAGIÁRIO BOOMER:
+#     def __str__(self):
+#         return f'''
+#         ESTAGIÁRIO BOOMER:
 
-        Personagem: {self.nome}
-        Idade: {self.idade}
-        Energia: {self.energia}
-        Dinheiro: {self.dinheiro}
-        Ansiedade: {self.ansiedade}
-        '''
+#         Personagem: {self.nome}
+#         Idade: {self.idade}
+#         Energia: {self.energia}
+#         Dinheiro: {self.dinheiro}
+#         Ansiedade: {self.ansiedade}
+#         '''
