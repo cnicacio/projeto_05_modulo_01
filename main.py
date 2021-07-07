e6d201266e7e1dfe70c492411677637b1e2a4519
lucascarneiro-dev <lucas.carneiro098@hotmail.com>
2021-07-06 21:14:28 -0300

    teste

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from character import Personagem
-from boomer import Boomer
-
 import functions
+from boomer import Boomer
+from character import Personagem
+from gen_z import Geracaoz
 
 boomer = Boomer('Carlos', 56, 50, 100, 50)
+genz   = Geracaoz('Enzo Gabriel', 18,100,50,100)
 
 print(boomer)
+print(genz)
